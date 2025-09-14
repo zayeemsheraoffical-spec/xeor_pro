64e277034554d8fb6c25b1a03e20d56e68055283
zayeemsheraoffical-spec <zayeemsheraoffical@gmail.com>
2025-09-14 14:33:56 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,14 @@
+package com.xeorpro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+import okhttp3.*
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Minimal starter
+    }
+}
